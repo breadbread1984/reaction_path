940d3440b8e5d4178f108dbe7297d9122d45f1e5
breadbread1984 <breadbread1984@163.com>
2024-04-26 16:35:16 +0800

    update

--- train.py
+++ train.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 
 from absl import flags, app
-from models import MaterialDecoder, PrecursorPredictor
 import torch
 from torch import nn
+from torch.utils.data import DataLoader
+from models import MaterialDecoder, PrecursorPredictor
+from datasets import MaterialDataset
 
 FLAGS = flags.FLAGS
 
 def add_options():
   flags.DEFINE_string('dataset', default = None, help = 'path to dataset')
   flags.DEFINE_string('ckpt', default = 'ckpt', help = 'path to ckpt')
+  flags.DEFINE_integer('batch_size', default = 32, help = 'batch size')
+  flags.DEFINE_integer('workers', default = 4, help = 'workers')
 
 def main(unused_argv):
-  mat_encoder = MaterialEncoder()
+  pre_predict = PrecursorPredictor()
+  mat_encoder = pre_predict.mat_encoder
   mat_decoder = MaterialDecoder()
-  pre_predict = PrecursorPredictor(mat_encoder = mat_encoder)
+  trainset = MaterialDataset(FLAGS.dataset, divide = 'train')
+  evalset = MaterialDataset(FLAGS.dataset, divide = 'val')
+  trainset_loader = DataLoader(trainset, batch_size = FLAGS.batch_size, shuffle = True, num_workers = FLAGS.workers)
   mse = nn.MSELoss()
 
 if __name__ == "__main__":
