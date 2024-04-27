c90bc216f3ed2cf82a100aec8dc39b7b65b12eea
yi.xie <yi.xie@enflame-tech.com>
2024-04-27 10:23:55 +0800

    update

---
+++ utils.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+import collections
+import numpy as np
+
+def get_composition_string(materials):
+  # convert an array of weights representing material
+  # to an array of strings representing material
+  comp_str = np.char.mod('%.6f', materials)
+  comp_str = np.array([' '.join(e) for e in comp_str])
+  return comp_str
+
+def convert_list_to_dico(all_labels, count_weights, num_reserved_ids = 10, least_count = 5):
+  out_labels = list()
+  out_counts = list()
+  if count_weights is None:
+    count_weights = [1.0] * len(all_labels)
+  else:
+    assert len(count_weights) == len(all_labels)
+
+  big_dico = collections.Counter()
+  for i in range(len(all_labels)):
+    w = count_weights[i]
+    s = all_labels[i]
+    if not isinstance(s, bytes):
+      s = bytes(s, encoding = 'utf-8')
+    big_dico[s] += w
+  big_dico = big_dico.most_common()
+  big_dico = list(filter(lambda x: x[1] >= least_count, big_dico))
+  print('len(big_dico)', len(big_dico))
+
+  if num_reserved_ids >= 2:
+    placeholder = ["<PLACEHOLDER>_{}".format(i) for i in range(num_reserved_ids)]
+    placeholder[0] = "<MASK>"
+    placeholder[1] = "<UNK>"
+    for p in reversed(placeholder):
+      big_dico.insert(0,(bytes(p, encoding="utf-8"),0,),)
+
+  out_labels = [x[0] for x in big_dico]
+  out_counts = [x[1] for x in big_dico]
+
+  return out_labels, out_counts
+
+def get_mat_dico(reactions, model = "all", num_reserved_ids = 10, least_count = 5):
+  mat_labels = list()
+  mat_compositions = list()
+  mat_counts = list()
+  all_mats = list() # all molecules appeared in the dataset
+  all_count_weights = list()
+  # put all precursors and all targets into list all_mats
+  for r in reactions:
+    if mode in {'all', 'target'}:
+      targets = r['target_comp']
+      all_mats.extend(targets)
+      all_count_weights.extend([r.get('count_weight', 1.0)] * len(targets))
+    if mode in {'all', 'precursor'}:
+      precursors = sum(r['precursors_comp'], [])
+      all_mats.extend(precursors)
+      all_count_weights.extend([r.get('count_weight', 1.0)] * len(precursors))
+  # convert all materials into strings
+  all_mats_str = get_composition_string(np.array(all_mats))
+  mat_str_comp = {s: comp for (s, comp) in zip(all_mats_str, all_mats)}
+  comp_shape = reactions[0]["target_comp"][0].shape
+
+  mat_labels, mat_counts = 
