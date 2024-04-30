b9f5b7c71a9c055c92cb36a43ec17dcd4b381f0b
breadbread1984 <breadbread1984@163.com>
2024-04-30 10:56:07 +0800

    update

--- recommendation.py
+++ recommendation.py
@@ -74,7 +74,7 @@ class PrecursorsRecommendation(object):
     formula = None if len(comp) == 0 else comp.get_integer_formula_and_factor(max_denominator = 1000000)[0]
     return formula
   def call(self, target_formula, top_n = 1, strategy = 'conditional', precursors_not_available = "default"):
-    assert stretegy in {'conditional', 'naive'}
+    assert strategy in {'conditional', 'naive'}
     if isinstance(target_formula, str):
       targets_formula = [target_formula]
     else:
