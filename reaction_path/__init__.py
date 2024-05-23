98609f876d8ca81d33edb4637bf94a2235e6947b
breadbread1984 <breadbread1984@163.com>
2024-05-23 15:06:58 +0800

    update

--- reaction_path/__init__.py
+++ reaction_path/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-from models import MaterialDecoder, PrecursorPredictor
-from recommendation import PrecursorsRecommendation
-from utils import get_ele_counts, get_composition_string, generate_labels
+from .models import MaterialDecoder, PrecursorPredictor
+from .recommendation import PrecursorsRecommendation
+from .utils import get_ele_counts, get_composition_string, generate_labels
