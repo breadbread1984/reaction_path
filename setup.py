f9acb1f404d4db338ee8f28eeb0ede74e4ea9a77
breadbread1984 <breadbread1984@163.com>
2024-06-17 17:18:03 +0800

    update

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(
+  name = "reaction_path",
+  version = "1.0",
+  author = "xieyi",
+  author_email = "yxie.lhi@gmail.com",
+  description = "a tool to prediction precursors of given material",
+  keywords = "material, reaction, precursor",
+  url = "https://github.com/breadbread1984/reaction_path/tree/main",
+  packages = ["reaction_path"],
+  install_requires = ["numpy","torch","transformers","pymatgen"],
+  license = "Apache License 2.0"
+)
